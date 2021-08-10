d77ae4ef8057d0bce0392dda34976e806b36eec9
Marc <52708150+marcpinet@users.noreply.github.com>
2021-08-10 20:31:57 +0200

    Reworked classes and methods and general improvements
    Made the downloader more "reusable" and "readable" + added some comments and really short for each classes/methods documentation

--- nyaa-downloader.py
+++ nyaa-downloader.py
@@ -19,49 +19,88 @@ qualities = [480, 720, 1080]
 
 
 #------------------------------CLASSES, METHODS & FUNCTIONS------------------------------
-
-
-class Anime:
+    
+    
+class Episode:
+    """Class that defines the episode of an anime by different attributes
     """
-    Class that defines anime by different attributes : name, quality, episodes.
-    """
-    numberOfAnime = 0
-    def __init__(self, name: str, quality: int, begin: int, end: int):
+    
+    
+    def __init__(self, name: str):
+        """Constructor of the Anime class.
+
+        Args:
+            name (str): Anime name. Defaults to 'UndefinedAnimeName'.
+        """
         self.name = name
-        self.quality = qualities[quality-1]
-        self.begin = begin
-        self.end = end
-        Anime.numberOfAnime += 1
-
-
-    def download(self, torrentName: str, downloadLink: str, animeName: str):
+        self.successful = 0
+    
+    
+    def download(self, torrentname: str, downloadlink: str):
         """Downloads a file from the web and saves it in a folder that has the name of the downloaded anime (the file also gets its original name).
         If you want more details, just read: https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
 
         Args:
-            torrentName (str): The name of the file to be saved.
-            downloadLink (str): The link of the file to download.
-            animeName (str): The name of the folder to create where the file will be saved (doesn't create it if already exists and move the file to the existing one).
+            torrentname (str: The name of the file to be saved.
+            downloadlink (str): The link of the file to download.
         """
         os.system('mkdir DownloadedTorrents > nul 2>&1')
-        os.system(f'mkdir \"DownloadedTorrents\\{animeName}\" > nul 2>&1')
+        os.system(f'mkdir \"DownloadedTorrents\\{self.name}\" > nul 2>&1')
         
-        with urllib.request.urlopen(downloadLink) as response, open(torrentName, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-            print(f"Downloading: {torrentName}, please wait...\n")
+        foldername = self.name
+                            
+        for s in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
+            foldername = foldername.replace(s, ' ')
+        
+        print(f"Downloading: {torrentname}, please wait...\n")
+        try :
+            with urllib.request.urlopen(downloadlink) as response, open(torrentname, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+            self.successful += 1
+        except:
+            print('Download failed. Please, check your internet connection.\n')
             
-        os.system(f"move \"{torrentName}\" \"DownloadedTorrents\\{animeName}\\{torrentName}\" > nul 2>&1")
+            
+        os.system(f"move \"{torrentname}\" \"DownloadedTorrents\\{foldername}\\{torrentname}\" > nul 2>&1")
 
 
-    def transfer(self, magnet: str, torrentName: str):
+    def transfer(self, torrentname: str, magnet: str):
         """Opens user's torrent client and transfers the file to it.
 
         Args:
+            torrentname (str): Torrent name to print to the console for the user to be able to see.
             magnet (str): Magnet link of the torrent to be downloaded.
-            torrentName (str): Torrent name to print to the console for the user to be able to see.
         """
-        print(f'Transferring to torrent client: {torrentName}, please wait...\n')
-        wb.open(magnet)
+        print(f'Transferring to torrent client: {torrentname}, please wait...\n')
+        try:
+            wb.open(magnet)
+            self.successful += 1
+        except:
+            print('Transfer failed. Please, make sure to have a torrent client or a web browser that supports magnet links!')
+
+
+class Batch:
+    """Class that defines a batch of anime.
+    """
+    
+    
+    numberOfBatchs = 0
+    def __init__(self, animename: str, quality: int, begin: int, end: int):
+        """Constructor of the Batch class.
+
+        Args:
+            animename (str): Name of the anime.
+            quality (int): Quality of the anime to be downloaded.
+            begin (int): Episode number of the first episode the user wants to download.
+            end (int): Episode number of the last episode the user wants to download.
+        """
+        self.animename = animename
+        self.episode = Episode(animename)
+        self.quality = qualities[quality-1]
+        self.begin = begin
+        self.end = end
+        Batch.numberOfBatchs += 1
+        
 
 #------------------------------MAIN FUNCTION------------------------------
 
@@ -93,6 +132,7 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
 """
         )
+        
         nextStep = input('Press ENTER to continue...')
         animeToDownload = []
         stillWantsAnime = True
@@ -160,7 +200,7 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
 
             # Finally, we append the class to the list of animes.
-            animeToDownload.append(Anime(animeName, qualityChoice, animeBegin, animeEnd))
+            animeToDownload.append(Batch(animeName, qualityChoice, animeBegin, animeEnd))
             os.system('cls')
             print(f'\n\nAlright, {animeName} from episode {animeBegin} to {animeEnd} will be downloaded in {qualities[qualityChoice-1]}p.')
 
@@ -198,32 +238,33 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
                 continue
             if answer not in [1, 2]:
                 print('Please, make sure to answer either 1 or 2.')
+                    
         verbalBase = 'downloaded' if answer == 1 else 'transferred'
 
 
         os.system('cls')
-        print('\n\nThe following anime will be downloaded:\n')
-        for anime in animeToDownload:
-            print('\t', anime.name, 'from episode', anime.begin, 'to', anime.end, 'in', str(anime.quality) + 'p')
+        
+        if Batch.numberOfBatchs == 1:
+            print('\n\nThe following anime will be downloaded:\n')
+        else:
+            print('\n\nThese {Batch.numberOfBatchs} anime will be downloaded:\n')
+        for batch in animeToDownload:
+            print('\t', batch.episode.name, 'from episode', batch.begin, 'to', batch.end, 'in', str(batch.quality) + 'p')
         nextStep = input('\n\nPress ENTER to continue...')
         os.system('cls')
-        
-        missingTorrents = []
 
 
         # For each anime the user has inputted...
-        for anime in animeToDownload:
+        for batch in animeToDownload:
+
 
             # In order to exit the loop when an episode with the 'END' tag is mentionned inside it (Erai-Raws does that, not SubsPlease)
             unexpectedEnd = False
 
-            # The number of downloaded episode of the currently checked anime (anime). Will be used to calculate percentage.
-            foundAnimes = 0
-
 
             # For each episode from the anime...
-            for i in range(anime.begin, anime.end + 1):
-                
+            for i in range(batch.begin, batch.end + 1):
+
 
                 # The program tries to retrieve from Nyaa.si the list of results for the anime and the corresponding episode. It often takes the most recent one.
                 for u in uploaders:
@@ -235,9 +276,8 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
                         epValue = '0' + str(i)
 
 
-                    foundTorrent = NyaaPy.Nyaa.search(keyword=f'[{u}] {anime.name} - {epValue} [{anime.quality}p]', category=1, subcategory=2, filters=2)
-                    print(f'Checking: [{u}] {anime.name} - {epValue} [{anime.quality}p]')
-                
+                    foundTorrent = NyaaPy.Nyaa.search(keyword=f'[{u}] {batch.episode.name} - {epValue} [{batch.quality}p]', category=1, subcategory=2, filters=2)
+                    print(f'Checking: [{u}] {batch.episode.name} - {epValue} [{batch.quality}p]')
 
                     # If at least one torrent has been found [...]
                     if len(foundTorrent) != 0:
@@ -245,39 +285,34 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
                         # We take the closest title to what we are looking for in order to avoid errors while browsing among every found torrents
                         torrent = None
                         for t in foundTorrent:
-                            if t['name'].lower().find(f'{anime.name} - {epValue}'.lower()) != -1:
+                            if t['name'].lower().find(f'{batch.episode.name} - {epValue}'.lower()) != -1:
                                 torrent = t
 
                         if torrent == None:
                             torrent = foundTorrent[0]
 
                         # We take the only two variables from the dictionary we are interested in (the result from the query stored in the variable foundTorrent)
-                        downloadLink = torrent['download_url']
-                        torrentName = torrent['name'] + '.torrent'
+                        downloadlink = torrent['download_url']
+                        torrentname = torrent['name'] + '.torrent'
                         magnet = torrent['magnet']
                         
                         # We put this variable to True so the program will continue until there is a result
                         stillFoundTorrents = True
-                        foundAnimes += 1
+                        
                         
                         # If the user chose .torrent option...
                         if answer == 1:
-                            # We download and then move the downloaded torrent to the dedicated folder.
-                            foldername = anime.name
-                            
-                            for s in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
-                                foldername = foldername.replace(s, ' ')
-
-                            anime.download(torrentName, downloadLink, foldername)
+                            # We download the .torrent file and save it in a folder which is in the same folder as where the script is running from.
+                            batch.episode.download(torrentname, downloadlink)
 
                         # If the user chose magnet option
                         else:
                             # We transfer it to a bittorent client which the user is supposed to have...
-                            anime.transfer(magnet, torrentName)
+                            batch.episode.transfer(torrentname, magnet)
                         
 
-                        if u == 'Erai-raws' and torrentName.find(' END [') != -1:
-                            print(f'Hey, {anime.name} has no more than {epValue} episodes!...\n')
+                        if u == 'Erai-raws' and torrentname.find(' END [') != -1:
+                            print(f'Hey, {batch.episode.name} has no more than {epValue} episodes!...\n')
                             unexpectedEnd = True
 
 
@@ -287,10 +322,15 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
                     # [...] Else, the program alerts you that no torrent have been found with the corresponding name.
                     else:
-                        print(f"No torrent found for the name: [{u}] {anime.name} - {epValue} [{anime.quality}p].\nEither it still hasn't aired or doesn't exist...\n")
+                        print(f"No torrent found for the name: [{u}] {batch.episode.name} - {epValue} [{batch.quality}p].\nEither it still hasn't aired or doesn't exist...\n")
                         
-                        if f'{anime.name} - Episode {epValue}' not in missingTorrents and u == uploaders[-1]:
-                            missingTorrents.append(f'{anime.name} - Episode {epValue}')
+                        # Just wanted to use that (lol) but it also makes the program use less RAM (like 8 bytes lmao)
+                        if 'missingTorrents' not in locals():
+                                missingTorrents = []
+                        
+                        # When uploaders reaches the index -1, it means that every uploader has been tried.
+                        if f'{batch.episode.name} - Episode {epValue}' not in missingTorrents and u == uploaders[-1]:
+                            missingTorrents.append(f'{batch.episode.name} - Episode {epValue}')
 
 
                 # So we don't check for inexistant title
@@ -299,14 +339,15 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
 
             if unexpectedEnd:
-                percentage = '0' if str(round(foundAnimes*100/(int(epValue) - anime.begin + 1), 2)).strip('0').strip('.') == '' else str(round(foundAnimes*100/(int(epValue) - anime.begin + 1), 2)).strip('0').strip('.')
+                percentage = '0' if str(round(batch.episode.successful*100/(int(epValue) - batch.begin + 1), 2)).strip('0').strip('.') == '' else str(round(batch.episode.successful*100/(int(epValue) - batch.begin + 1), 2)).strip('0').strip('.')
 
             else:
-                percentage = '0' if str(round(foundAnimes*100/(anime.end - anime.begin + 1), 2)).strip('0').strip('.') == '' else str(round(foundAnimes*100/(anime.end - anime.begin + 1), 2)).strip('0').strip('.')
+                percentage = '0' if str(round(batch.episode.successful*100/(batch.end - batch.begin + 1), 2)).strip('0').strip('.') == '' else str(round(batch.episode.successful*100/(batch.end - batch.begin + 1), 2)).strip('0').strip('.')
+            
             
             # This notifies you that every torrent from an anime has been fully downloaded.
             toaster = win10toast.ToastNotifier()
-            toaster.show_toast('Nyaa Auto-download', f'The anime {anime.name} has been {verbalBase} at {percentage}%!')
+            toaster.show_toast('Nyaa Auto-download', f'The anime {batch.episode.name} has been {verbalBase} at {percentage}%!')
             percentageStock = []
             percentageStock.append(percentage == '100')
 
@@ -318,11 +359,13 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
     # Creating Logs if missing episodes. Else, do nothing but tells the user that everything went well.
     if all(percentageStock):
-        print(f'\nEvery anime have been fully {verbalBase}!\n')
+        
+        print(f'\nEvery anime has been fully {verbalBase}!\n')
         if verbalBase == 'downloaded':
             exitVariable = input('Press enter to exit and open the folder containing .torrent files...')
         else:
             exitVariable = input('Press enter to exit...')
+    
     else:
         print("\n\nThe episodes that couldn't be downloaded have been stored in a .txt file.")
         print('You can access it by going into the same folder as the python script.')
@@ -333,8 +376,8 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
         with open('missingEpisodes.txt', 'w') as logs:
             logs.write("The following episodes couldn't be downloaded:\n\n" + text + '\n')
 
-
         print('\nDo wou want to open the .txt file from there or exit the program? (1=.txt, 2=exit)')
+        
         openTxtOrExit = 0
         while openTxtOrExit not in [1, 2]:
             try:
@@ -365,6 +408,7 @@ If you find any bug, please make me know on my GitHub ~~> https://github.com/mar
 
 
 #------------------------------MAIN CALL------------------------------
-            
+
+
 if __name__ == '__main__':
     main()
